25d3a141a9859a40ec0e1f4906d836cc0979dd96
Eduardo <eduardoviniciusteixeira@gmail.com>
2023-11-28 18:12:36 -0300

    Create function to determine who wins, add an Array to track the choosen spots and winning message from whoever won

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ const Game = (() => {
   const board = document.getElementById('board');
   const modalSelectPlayer = document.getElementById('selectBox');
   const cellsElements = document.querySelectorAll('[data-cell]');
+  const resultBox = document.getElementById('winningMessage');
+  const winningMessage = document.querySelector('[data-winning-message-text]')
   const btnPlayers = document.getElementById('players');
   const winCombos =[
     [0,1,2],
@@ -27,12 +29,38 @@ const Game = (() => {
     })
 
     cellsElements.forEach(cell => {
-      cell.addEventListener('click', handleClick, {once:true})
+      cell.addEventListener('click', handleClick, {once:true});
     })
   }
 
   const handleClick = (square) => {
-    console.log(square.target.id)
+    turn(square.target,square.target.id, Hplayer);
+  }
+
+  const turn = (squareClass,squareId, player) => {
+    origBoard[squareId] = player;
+    squareClass.classList.add(player);
+    let gameWon = checkWin(origBoard, player);
+
+    if(gameWon) gameOver(gameWon)
+  }
+
+  const checkWin = (board, player) => {
+    let plays = board.reduce((a, e, i) => 
+      ((e === player)) ? a.concat(i) : a, []);
+    let gameWon = null;
+    for(let [index, win] of winCombos.entries()) {
+      if(win.every(elem => plays.indexOf(elem) > -1)) {
+        resultBox.classList.add('show')
+        gameWon = {player: player}
+        break;
+      }
+    }
+    return gameWon;  
+  }
+
+  const gameOver = (gameWon) => {
+    winningMessage.innerHTML = `${gameWon.player === "circle" ? 'O' : 'X'}'s wins!!`
   }
 
   return {
